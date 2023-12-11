94f37050c9565fee22cba890bbb76b883ae765cd
Andres Villavicencio Escorcia <andres.villavicencio@wizeline.com>
2023-12-11 00:32:59 -0600

    Remove budget from query and add documentation to readme

--- capstone_project/main.py
+++ capstone_project/main.py
@@ -14,14 +14,13 @@ if  __name__ == "__main__":
     text_deck = deck.deck_list_to_string()
 
     aspect = input("What aspect of your deck you want to improve: ")
-    budget = input("How much do you want to spend on these upgrades: ")
+    # budget = input("How much do you want to spend on these upgrades: ")
     
     response = bot.request_openai(f"""
                                 Suggest me list of 5 Magic the gathering cards in json format, that would upgrade my 
                                 {commander} commander deck. And what cards to replace from the deck.
                                 The deck contains this list of cards: {text_deck}, and the main focus on the upgrades should be on {aspect}. 
-                                With a {budget} budget
-                                  """)
+                                """)
     # parse the response:
     cards = json.loads(response)
 
